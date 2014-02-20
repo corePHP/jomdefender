0f3137c8e524bfb2b967a8ae01900b2cc0c5da3d
Collin Ferguson <collin@corephp.com>
2014-02-20 14:24:41 +0000

    Fixing issue with NULL users editing from front end
    jomdefender.php edited online with Bitbucket

--- 25/src/plugins/system/jomdefender/jomdefender.php
+++ 25/src/plugins/system/jomdefender/jomdefender.php
@@ -299,7 +299,7 @@ class plgSystemJomDefender extends JPlugin {
 		// If anyone things of a better way let me know.
 		// It is all about having the Joomla word repeat inside of the html tags
 		if ($this->_params->get('remove_joomla') && $this->_is_site
-				&& !$this->_user->authorise('can.edit', 'com_content')) {
+				&& $this->_user->authorise('can.edit', 'com_content') === 0) {
 			if (preg_match(
 					'/(?:(?<=\>)|(?<=\/\>))(\s*?.*?)(joomla(!)?)(\s*?.*?)(?=\<\/?)/i',
 					$data)) {
